ca1f880dfcca7849b8c9046662d31c182bb3d515
laxitshahi <shahilax@my.yorku.ca>
2024-02-22 04:19:50 -0500

    finished substring assignment

--- basic-projects/substring.rb
+++ basic-projects/substring.rb
@@ -1,14 +1,13 @@
-def substring(_word, substrings)
+def substrings(word, substrings)
   op_hash = {}
   substrings.each do |substring|
-    if substring in op_hash
-      # op_hash[substring] += 1
-    else
-      op_hash[substring] = 1
-    end
+    op_hash[substring] = word.downcase.scan(substring.downcase).length if word.downcase.include?(substring.downcase)
   end
 
-  p op_hash.keys
+  p op_hash
 end
 
-substring('hello', %w[hello bye hell sdkfjs hell bye])
+substrings('hello', %w[hello bye hell hell bye])
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+substrings('below', dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
